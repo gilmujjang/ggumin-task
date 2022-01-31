51a0cedab4824c9f7514428d154d979e04b6388b
gilmujjang <alsanrlf@naver.com>
2022-01-31 21:22:33 +0900

    [chore] 사용하지 않는 파일 삭제 & eslint, prettier 설정

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports ={
+  "plugins": ["prettier", "@typescript-eslint"],
+  "extends": ["airbnb-typescript", "react-app", "prettier"],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "project": "./tsconfig.json"
+  },
+  "rules": {
+    "import/no-cycle": 0,
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
+  }
+}

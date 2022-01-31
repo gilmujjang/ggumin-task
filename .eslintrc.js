671927aadcb47f09fd58fcca9a48da5f2f0bd759
gilmujjang <alsanrlf@naver.com>
2022-01-31 21:36:43 +0900

    [chore] 라이브러리 설치

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,17 @@
-module.exports ={
-  "plugins": ["prettier", "@typescript-eslint"],
-  "extends": ["airbnb-typescript", "react-app", "prettier"],
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "project": "./tsconfig.json"
+module.exports = {
+  plugins: ['prettier', '@typescript-eslint'],
+  extends: ['airbnb-typescript', 'react-app', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
   },
-  "rules": {
-    "import/no-cycle": 0,
-    "prettier/prettier": [
-      "error",
+  rules: {
+    'import/no-cycle': 0,
+    'prettier/prettier': [
+      'error',
       {
-        endOfLine: "auto",
+        endOfLine: 'auto',
       },
     ],
-  }
-}
+  },
+};
